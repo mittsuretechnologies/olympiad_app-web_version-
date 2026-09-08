9616fdad41be4fc35bcb5d3ebfa33acd65660156
mittsuretechnologies <mittsuretechnologies@gmail.com>
2026-09-08 11:39:54 +0530

    .........................
    ...........

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -5,7 +5,8 @@ import { FlatCompat } from "@eslint/eslintrc";
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
-.  baseDirectory: __dirname,
+const compat = new FlatCompat({
+  baseDirectory: __dirname,
 });
 
 const eslintConfig = [
